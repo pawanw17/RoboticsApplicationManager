6756107a00da7bc12ee56b9e72bce4deee99bbc4
pawanw17 <pawanw17@gmail.com>
2023-08-09 19:40:58 +0530

    Added aerostack and px4 Ilaunchers

---
+++ manager/manager/launcher/launcher_px4.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+from src.manager.manager.launcher.launcher_interface import ILauncher, LauncherException
+from src.manager.manager.docker_thread.docker_thread import DockerThread
+from typing import List, Any
+import psutil
+import threading
+
+class LauncherPx4(ILauncher):
+    exercise_id: str
+    type: str
+    module: str
+    parameters: List[str]
+    launch_file: str
+    process: Any = None
+    thread: Any = None
+
+    def run(self, callback: callable = None):
+        self.launch_file = os.path.expandvars(self.launch_file)
+
+        # Start the process in a separate thread
+        self.thread = DockerThread(f'ros2 launch {self.launch_file}')
+        self.thread.start()
+
+    def is_running(self):
+        return True
+
+    def terminate(self):
+        self.thread.terminate()
+        self.thread.join()
+
+    def died(self):
+        pass
